8b879b048f0cd2f45fca5bb8cd5288a3df12172b
giangann <70595717+giangann@users.noreply.github.com>
2022-03-25 14:28:51 +0700

    google auth sample

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtvoJ2uvkp0AxXOtAwfhzTnhk8CpSMnSU",
+  authDomain: "fir-auth-920cb.firebaseapp.com",
+  projectId: "fir-auth-920cb",
+  storageBucket: "fir-auth-920cb.appspot.com",
+  messagingSenderId: "376716459774",
+  appId: "1:376716459774:web:bc320172c3c69624a1efaf",
+  measurementId: "G-8L7NRPBMZK",
+};
+
+export default firebaseConfig;
